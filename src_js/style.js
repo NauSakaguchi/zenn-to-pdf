3afc0acb5400c877cb4ee71bb5663c9d13cb8c8c
Nau Sakaguchi <nau.sakaguchi@sjsu.edu>
2021-08-28 18:22:47 +0900

    impl javascript to modify the style of pages for print

--- src_js/style.js
+++ src_js/style.js
@@ -5,3 +5,15 @@ for (let i = 0; i < list_items.length; i++) {
     list_items[i].setAttribute("style", "display: none");
 }
 
+list_items = document.getElementsByTagName('aside');
+console.log(list_items[0]);
+for (let i = 0; i < list_items.length; i++) {
+    console.log(list_items[i])
+    list_items[i].setAttribute("style", "display: none");
+}
+
+list_items = document.getElementsByTagName('section')
+for (let i = 0; i < list_items.length; i++) {
+    console.log(list_items[i])
+    list_items[i].setAttribute("style", "padding-left: 0");
+}
