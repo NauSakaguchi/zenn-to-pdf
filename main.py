d36134be88684285e7fae554f851a304160ec087
Nau Sakaguchi <nau.sakaguchi@sjsu.edu>
2021-08-29 00:28:52 +0900

    can save each chapter contents as pdf files

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 # This is a zenn_to_pdf Python script.
+import os
+import shutil
+
 import time
 
 from selenium import webdriver
 
-from utils import js_to_text
+from pdf_downloader import get_chrome_options, save_as_pdf
+
+# todo
+# url_articleを入力された値にする
+# pdfで各チャプターを保存する
+# 各チャプターのPDFを一つにまとめあげる
 
 
 if __name__ == '__main__':
@@ -13,11 +21,21 @@ if __name__ == '__main__':
     class_name_of_table_contents = 'ChapterRow_link__14dfi'
     url_list_table_contents = []
     url_table_contents = ''
-    output_filepath = './output/test.html'
+    chapter_title_list = []
+    output_filepath = './output'
     style_file_path = 'src_js/style.js'
+    driver_path = '/usr/local/bin/chromedriver'
+
+    # clean output folder
+    target_dir = output_filepath
+    shutil.rmtree(target_dir)
+    os.mkdir(target_dir)
+    os.mkdir(target_dir + '/src')
 
     # setup web driver for scraping
-    driver = webdriver.Chrome('/usr/local/bin/chromedriver')  # Optional argument, if not specified will search path.
+    savefile_default_directory = os.path.dirname(__file__) + '/output/src'
+    chrome_options = get_chrome_options(savefile_default_directory)
+    driver = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
     driver.get(url_article)
 
     # get URL list of the table contents
@@ -26,14 +44,10 @@ if __name__ == '__main__':
         print(contents.get_attribute('href'))
         url_list_table_contents += [contents.get_attribute('href')]
 
-    # access to the contents pages
-    url_table_contents = url_list_table_contents[0]
-    driver.get(url_table_contents)
-
-    # manipulate html tag
-    js = js_to_text(style_file_path)
-    driver.execute_script(js)
+    # access to the contents pages & save as pdf
+    for url_table_contents in url_list_table_contents:
+        chapter_title_list.append(save_as_pdf(driver, url_table_contents, style_file_path))
 
     # close chrome window and terminate driver
-    time.sleep(5)
     driver.quit()
+
---
+++ pdf_downloader.py
@@ -0,0 +1,56 @@
+import time
+
+from selenium import webdriver
+import json
+
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+from utils import js_to_text
+
+
+def get_chrome_options(savefile_default_directory):
+    # 印刷としてPDF保存する設定
+    chrome_options = webdriver.ChromeOptions()
+    app_state = {
+        "recentDestinations": [
+            {
+                "id": "Save as PDF",
+                "origin": "local",
+                "account": ""
+            }
+        ],
+        "selectedDestinationId": "Save as PDF",
+        "version": 2,
+        "isLandscapeEnabled": False,  # 印刷の向きを指定 tureで横向き、falseで縦向き。
+        "pageSize": 'A4',  # 用紙タイプ(A3、A4、A5、Legal、 Letter、Tabloidなど)
+        "marginsType": 0,  # 余白タイプ #0:デフォルト 1:余白なし 2:最小
+        # "scalingType": 3 , #0：デフォルト 1：ページに合わせる 2：用紙に合わせる 3：カスタム
+        # "scaling": "141" ,#倍率
+        "isHeaderFooterEnabled": False,  # ヘッダーとフッター
+        "isCssBackgroundEnabled": True,  # 背景のグラフィック
+    }
+
+    prefs = {'printing.print_preview_sticky_settings.appState': json.dumps(app_state),
+             # "download.default_directory": "/Users/nau/PycharmProjects/zenn_to_pdf/output",
+             "savefile.default_directory": savefile_default_directory,
+             # "download.prompt_for_download": False,  # To auto download the file
+             # "download.directory_upgrade": True,
+             # "plugins.always_open_pdf_externally": True  # It will not show PDF directly in chrome
+             }
+    chrome_options.add_experimental_option('prefs', prefs)
+    chrome_options.add_argument('--kiosk-printing')  # 印刷ダイアログが開くと、印刷ボタンを無条件に押す。
+    return chrome_options
+
+
+def save_as_pdf(driver, url_table_contents, style_file_path):
+    driver.get(url_table_contents)
+    # manipulate html tag
+    pdf_title = driver.title + '.pdf'
+    js = js_to_text(style_file_path)
+    driver.execute_script(js)
+    WebDriverWait(driver, 15).until(
+        expected_conditions.presence_of_all_elements_located)  # ページ上のすべての要素が読み込まれるまで待機（15秒でタイムアウト判定）
+    time.sleep(1)  # これがないと画像の表示が乱れることアリ
+    driver.execute_script('return window.print()')  # Print as PDF
+    return pdf_title
